1ea24af1b7807a7363f75b4aef580a6414dd3d05
youngff43 <99932948+youngff43@users.noreply.github.com>
2022-06-26 01:39:20 -0400

    add handlebars pages

---
+++ controllers/home-routes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    // show the main.handlebars template along with all data 
+    Post.findAll({
+        attributes: ['id', 'post_url', 'title', 'created_at' ],
+        include: [
+            {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+              }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        // loops over and serialize the entire array of posts 
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        // pass the posts object into the homepage template
+        res.render('homepage', { posts });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+// makes the stylesheet available to the client
+const path = require('path');
+// set up handlebars as template engine 
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// middleware that serves all files in the public folder as static assets 
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
